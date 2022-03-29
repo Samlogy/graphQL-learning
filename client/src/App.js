e2e634c76c125cf7d5a0c218458181712d84c097
Samy Senani <samy.senani@deepor.ai>
2022-03-29 16:46:12 +0200

    add: create product

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import { ApolloClient, ApolloProvider, InMemoryCache, useQuery } from "@apollo/client";
-import { GET_ALL_PRODUCTS, GET_PRODUCT_BY_ID } from "./services";
+import { ApolloClient, ApolloProvider, InMemoryCache, useQuery, useMutation } from "@apollo/client";
+import { GET_ALL_PRODUCTS, GET_PRODUCT_BY_ID, CREATE_PRODUCT } from "./services";
 
 function App() {
   const client = new ApolloClient({
@@ -12,7 +12,12 @@ function App() {
       <div className="App">
         <h2> Products </h2>
         <Products />
+
+        <h2> Product </h2>
         <Product id="2c931e7e-510f-49e5-aed6-d6b44087e5a1" />
+
+        <h2> Create Product </h2>
+        <CreateProduct />
       </div>
     </ApolloProvider>
   );
@@ -32,7 +37,7 @@ const Products = () => {
   return (
     data.products &&
     data.products.map(({ id, name, onSale, categoryId, price }) => (
-      <div style={{ padding: ".5rem", margin: ".5rem", border: "1px solid", borderRadius: "5px", maxWidth: 'fit-content' }} key={id}>
+      <div style={{ padding: ".5rem", margin: ".5rem", border: "1px solid", borderRadius: "5px", maxWidth: "fit-content" }} key={id}>
         <div> {id} </div>
         <div> {name} </div>
         <div> {onSale} </div>
@@ -44,23 +49,38 @@ const Products = () => {
 };
 
 const Product = ({ id }) => {
-  const { data, loading, error } = useQuery(GET_PRODUCT_BY_ID);
+  const { data, loading, error } = useQuery(GET_PRODUCT_BY_ID, {
+    variables: { id: id },
+  });
 
   if (loading) return <p> Loading... </p>;
   if (error) return <p>ERROR</p>;
   if (!data) return <p>Not found</p>;
 
-  console.log({ data, loading, error });
+  // console.log(data.product);
 
   return (
-
-      <div style={{ padding: ".5rem", margin: ".5rem", border: "1px solid", borderRadius: "5px", maxWidth: 'fit-content' }} >
-        <div> {data.id} </div>
-        {/* <div> {name} </div>
-        <div> {onSale} </div>
-        <div> {price} </div>
-        <div> {categoryId} </div> */}
-      </div>
+    <div style={{ padding: ".5rem", margin: ".5rem", border: "1px solid", borderRadius: "5px", maxWidth: "fit-content" }}>
+      <div> {data.product.id} </div>
+      <div> {data.product.name} </div>
+      <div> {data.product.onSale} </div>
+      <div> {data.product.price} </div>
+      <div> {data.product.categoryId} </div>
+    </div>
   );
 };
 
+const CreateProduct = () => {
+  const [create, { loading, error }] = useMutation(CREATE_PRODUCT);
+
+  if (loading) return <p> Loading... </p>;
+  if (error) return <p>An error occurred</p>;
+
+  console.log(create);
+
+  return (
+    <div style={{ padding: ".5rem", margin: ".5rem", border: "1px solid", borderRadius: "5px", maxWidth: "fit-content" }}>
+      <form>here</form>
+    </div>
+  );
+};
--- client/src/services.js
+++ client/src/services.js
@@ -1,31 +1,41 @@
 import { gql } from "@apollo/client";
 
-
 export const GET_ALL_PRODUCTS = gql`
   query Query {
-  products(filter: {onSale: true}) {
-    id
-    name
-    description
-    quantity
-    price
-    image
-    onSale
-    categoryId
+    products(filter: { onSale: true }) {
+      id
+      name
+      description
+      quantity
+      price
+      image
+      onSale
+      categoryId
+    }
   }
-}
-`
+`;
 export const GET_PRODUCT_BY_ID = gql`
-query Query($id: ID!) {
-  product(id: $id) {
-    name
-    description
-    quantity
-    price
-    image
-    onSale
-    categoryId
-    id
+  query Query($id: ID!) {
+    product(id: $id) {
+      name
+      description
+      quantity
+      price
+      image
+      onSale
+      categoryId
+      id
+    }
   }
-}
+`;
+
+export const CREATE_PRODUCT = gql`
+  mutation Mutation($input: AddProductInput!) {
+    addProduct(input: $input) {
+      name
+      id
+      onSale
+      price
+    }
+  }
 `;
