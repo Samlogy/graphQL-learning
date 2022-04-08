989237c892373473c29c8001ac7052bd40584225
Samy Senani <samy.senani@deepor.ai>
2022-04-08 11:54:17 +0200

    edit: upload single / multiple files --> add drap & drop zone --> react drop

--- client/src/components/UploadFile.js
+++ client/src/components/UploadFile.js
@@ -1,10 +1,12 @@
-import { useState, useEffect } from "react";
+import React, {useCallback, useState, useEffect} from 'react'
+import {useDropzone} from 'react-dropzone'
 import { useMutation } from "@apollo/client";
+
 import { SINGLE_UPLOAD, MULTIPLE_UPLOAD } from '../services'
 
 export const UploadFile = () => {
   return (
-    <div style={{ border: '1px solid', width: 'fit-content', padding: '.5rem', borderRadius: '5px' }}>
+    <div >
       <UploadSingle />
       <UploadMultiple />
     </div>
@@ -12,22 +14,24 @@ export const UploadFile = () => {
 };
 
 const UploadSingle = () => {
-  const [lastUploaded, setLastUploaded] = useState({});
   const [uploadSingleFile, { loading, error, data }] = useMutation(SINGLE_UPLOAD, {
     onCompleted: (data) => console.log('success: ', data),
     onError: (data) => console.log('error: ', data)
   });
 
-  const onUploadSingleFile = (e) => {
-    const file = e.target.files[0];
-    if (!file) return 
-    uploadSingleFile({ variables: { file } })
-  }
+  const onDrop = useCallback(acceptedFile => {
+    console.log(acceptedFile)
+    if (acceptedFile) uploadSingleFile({ variables: { file: acceptedFile[0] } })
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop, multiple: false})
 
-  // useEffect(() => {
-  //   if (data)
-  //     setLastUploaded(data.singleUpload);
-  // }, [data]);
+  // const onUploadSingleFile = (e) => {
+  //   const file = e.target.files[0];
+  //   if (!file) return 
+  //   uploadSingleFile({ variables: { file } })
+  // }
+
+  console.log('loading: ', loading)
 
   if (loading)
     return <div>Loading...</div>;
@@ -37,8 +41,16 @@ const UploadSingle = () => {
 
   return(
     <>
-      <h2> Upload Single File </h2>
-      <input type="file" required onChange={onUploadSingleFile} />
+      <h3> Upload Single File </h3>
+      {/* <input type="file" required onChange={onUploadSingleFile} /> */}
+      <div {...getRootProps()} style={{ border: '1px solid', width: 'fit-content', padding: '.5rem', borderRadius: '5px' }}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+            <p>Drop the files here ...</p> :
+            <p>Drag 'n' drop some files here, or click to select files</p>
+        }
+      </div>
       <br />
       {/* {Object.keys(lastUploaded).length !== 0 && (
         <div>
@@ -51,40 +63,41 @@ const UploadSingle = () => {
 }
 
 const UploadMultiple = () => {
-  const [lastUploaded, setLastUploaded] = useState({});
-  // , { loading, error, data }
   const [uploadMultipleFiles, { loading, error, data }] = useMutation(MULTIPLE_UPLOAD, {
     onCompleted: (data) => console.log('succes: ', data),
     onError: (data) => console.log('error: ', data)
   });
 
-  const onUploadMultipleFiles = (e) => {
-    const file = e.target.files
-    if (!file || file.length === 0) return 
-    uploadMultipleFiles({ variables: { file } })
-  }
+  // const onUploadMultipleFiles = (e) => {
+  //   const file = e.target.files
+  //   if (!file || file.length === 0) return 
+  //   uploadMultipleFiles({ variables: { file } })
+  // }
 
-  // useEffect(() => {
-  //   if (data)
-  //     setLastUploaded(data.singleUpload);
-  // }, [data]);
+  const onDrop = useCallback(acceptedFiles => {
+    if (acceptedFiles) uploadMultipleFiles({ variables: { file: acceptedFiles } })
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
   if (loading)
     return <div>Loading...</div>;
-  // if (error)
-  //   return <div>{JSON.stringify(error, null, 2)}</div>;
+
+  if (error)
+    return <div>{JSON.stringify(error, null, 2)}</div>;
 
   return(
     <>
-      <h2> Upload Multiple Files </h2>
-      <input type="file" required onChange={onUploadMultipleFiles} multiple={true} />
+      <h3> Upload Multiple Files </h3>
+      {/* <input type="file" required onChange={onUploadMultipleFiles} multiple={true} /> */}
+      <div {...getRootProps()} style={{ border: '1px solid', width: 'fit-content', padding: '.5rem', borderRadius: '5px' }}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+            <p>Drop the files here ...</p> :
+            <p>Drag 'n' drop some files here, or click to select files</p>
+        }
+      </div>
       <br />
-      {/* {Object.keys(lastUploaded).length !== 0 && (
-        <div>
-          {" "}
-          Last uploaded details => {JSON.stringify(lastUploaded, null, 2)}{" "}
-        </div>
-      )} */}
     </>
   )
 }
